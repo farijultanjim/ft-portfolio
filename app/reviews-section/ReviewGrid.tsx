a9296de4822a3c1edea3de0fdd3b14439d5b5d2a
Farijul Tanzil <farijul35-2888@diu.edu.bd>
2024-11-09 23:21:07 +0600

    review section added

--- app/reviews-section/ReviewGrid.tsx
+++ app/reviews-section/ReviewGrid.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import ReviewCard from "./ReviewCard";
+import AnimatedBody from "../animations/AnimatedBody";
+import AnimatedWords2 from "../animations/AnimatedWords2";
+import { monaSans } from "../fonts/monaSans";
 
 const ReviewGrid = () => {
   return (
-    <div>
-      ReviewGrid
-    </div>
-  )
-}
+    <section className="z-10 flex w-full flex-col items-center justify-center overflow-hidden bg-[#0E1016] bg-cover bg-center pt-20 md:pb-16 lg:pb-24">
+      <div
+        className={`relative mb-16 flex w-full flex-col items-center justify-center gap-10 text-[#e4ded7] sm:items-center lg:max-w-[1440px]`}
+      >
+        <AnimatedWords2
+          title="Reviews"
+          style={`flex max-w-[500px] flex-col items-center text-left pr-5 ${monaSans.className} font-extrabold uppercase leading-[0.9em] text-[#e4ded7] sm:max-w-full sm:flex-row sm:items-center sm:justify-center sm:text-center lg:text-center text-[clamp(70px,4vw,155.04px)]`}
+        />
 
-export default ReviewGrid
+        <AnimatedBody
+          text="Access top-tier group mentoring plans and exclusive professional benefits for you team"
+          className="w-[90%] text-center text-sm font-semibold uppercase sm:w-[500px] md:w-[550px] md:text-base"
+        />
+      </div>
+      <ReviewCard />
+    </section>
+  );
+};
+
+export default ReviewGrid;
--- app/work-section/ProjectCard.tsx
+++ app/work-section/ProjectCard.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import { ProjectProps } from "./projectDetails";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 import AnimatedTitle from "../animations/AnimatedTitle";
 import AnimatedBody from "../animations/AnimatedBody";
@@ -13,6 +13,13 @@ const ProjectCard = ({
   image,
 }: ProjectProps) => {
   const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"],
+  })
+
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1])
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1])
 
   return (
     <motion.div
@@ -23,6 +30,8 @@ const ProjectCard = ({
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center center",
         position: "relative",
+        scale: scaleProgress,
+        opacity: opacityProgress
       }}
       className={`relative z-10 h-[550px] w-full items-stretch justify-center overflow-hidden rounded-3xl bg-center py-0 sm:h-[700px] sm:w-[100%] md:h-[550px] md:w-[100%] lg:h-[400px]`}
       initial="initial"
@@ -31,33 +40,43 @@ const ProjectCard = ({
       <Image
         src={image}
         alt={name}
-        className={`absolute -bottom-2 w-[70%] sm:w-[85%] md:w-[60%] lg:max-w-[60%] ${id % 2 === 0 ? "right-0" : "left-0"}`}
+        className={`absolute -bottom-2 w-[70%] sm:w-[85%] md:w-[60%] lg:max-w-[60%] ${
+          id % 2 === 0 ? "right-0" : "left-0"
+        }`}
       />
 
-      <AnimatedTitle
-        text={name}
-        className={
-          "lg:max-w-w[450px] lg:text-[48px lg:leading-none] max-w-[90%] text-[40px] leading-none text-white md:text-[44px] md:leading-none"
-        }
-        wordSpace={"mr-[0.25em]"}
-        charSpace={"-mr-[0.01em]"}
-      />
+      <div
+        className={`absolute text-white ${
+          !(id % 2 === 0)
+            ? "right-0 top-20 mr-0 md:right-0 md:ml-0 lg:right-0 lg:top-20 lg:mr-4"
+            : "left-10 top-20 ml-0 md:mr-12 lg:top-20 lg:ml-4"
+        } mb-10 md:mb-16 lg:mb-14`}
+      >
+        <AnimatedTitle
+          text={name}
+          className={
+            "lg:max-w-w[450px] lg:text-[48px lg:leading-none] max-w-[90%] text-[40px] leading-none text-white md:text-[44px] md:leading-none"
+          }
+          wordSpace={"mr-[0.25em]"}
+          charSpace={"-mr-[0.01em]"}
+        />
 
-      <AnimatedBody
-        text={description}
-        className="mt-4 w-[90%] max-w-[457px] text-base font-semibold text-[#95979D]"
-      />
+        <AnimatedBody
+          text={description}
+          className="mt-4 w-[90%] max-w-[457px] text-base font-semibold text-[#95979D]"
+        />
 
-      <div className="mt-9 flex gap-4">
-        {technologies.map((tech, id) => (
-          <AnimatedTitle
-            text={tech}
-            wordSpace={"mr-[0.25em]"}
-            charSpace={"mr-[0.01em]"}
-            key={id}
-            className={"text-sm font-bold uppercase md:text-base lg:text-lg"}
-          />
-        ))}
+        <div className="mt-9 flex gap-4">
+          {technologies.map((tech, id) => (
+            <AnimatedTitle
+              text={tech}
+              wordSpace={"mr-[0.25em]"}
+              charSpace={"mr-[0.01em]"}
+              key={id}
+              className={"text-sm font-bold uppercase md:text-base lg:text-lg"}
+            />
+          ))}
+        </div>
       </div>
     </motion.div>
   );
